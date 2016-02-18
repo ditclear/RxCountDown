096b16cba1a00cd469d651149f68f5741b73cf64
vienan <vienan@163.com>
2016-02-18 15:57:51 +0800

    update kt file

--- app/src/main/java/vienan/app/countdown/MainActivity.kt
+++ app/src/main/java/vienan/app/countdown/MainActivity.kt
@@ -22,7 +22,7 @@ import rx.functions.Func1
 import rx.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
-    internal var subscriptionCountDown: Subscription ?=null
+    internal var subscriptionCountDown: Subscription ? = null
     //    DateTime currentDateTime,targetDateTime;
 
     //    String currentDate;
@@ -45,37 +45,38 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setUpView() {
-        var tvCountDown: TextView =findViewById(R.id.tv_countdown) as TextView
-        subscriptionCountDown = Observable.interval(1, TimeUnit.SECONDS, Schedulers.computation()).map { s ->
-            Log.d("s", "" + s!!)
-            DateTime(2016, 2, 25, 11, 2, 0)
-        }.map { dateTime -> Interval(DateTime.now(), dateTime).toPeriod() }.observeOn(AndroidSchedulers.mainThread()).subscribe(object : Observer<Period> {
-            override fun onCompleted() {
-                Log.d("onCompleted", "task is completed")
+        var tvCountDown: TextView = findViewById(R.id.tv_countdown) as TextView
+        subscriptionCountDown =
+                Observable.interval(1, TimeUnit.SECONDS, Schedulers.computation())
+                        .map { s -> DateTime(2016, 2, 25, 11, 2, 0) }
+                        .map { dateTime -> Interval(DateTime.now(), dateTime).toPeriod() }
+                        .observeOn(AndroidSchedulers.mainThread()).subscribe(object : Observer<Period> {
+                    override fun onCompleted() {
+                        Log.d("onCompleted", "task is completed")
 
-            }
+                    }
 
-            override fun onError(e: Throwable) {
-                Log.d("onError", "something went wrong in TimingDemoFragment example:" + e.message)
-                tvCountDown?.text = "倒计时结束"
-                toast("倒计时结束")
-            }
+                    override fun onError(e: Throwable) {
+                        Log.d("onError", "something went wrong in TimingDemoFragment example:" + e.message)
+                        tvCountDown?.text = "倒计时结束"
+                        toast("倒计时结束")
+                    }
 
-            override fun onNext(period: Period) {
-                Log.d("onNext", "show countdown here")
-                val countDownRText = "剩 " + period.days + "天" + period.hours + "时" + period.minutes + "分"+period.seconds + "秒"
-                tvCountDown?.text = countDownRText
-            }
-        })
+                    override fun onNext(period: Period) {
+                        Log.d("onNext", "show countdown here")
+                        val countDownRText = "剩 " + period.days + "天" + period.hours + "时" + period.minutes + "分" + period.seconds + "秒"
+                        tvCountDown?.text = countDownRText
+                    }
+                })
 
     }
 
     override fun onDestroy() {
         super.onDestroy()
-//        subscriptionCountDown?.unsubscribe()
+        //        subscriptionCountDown?.unsubscribe()
     }
 
-    fun MainActivity.toast(message:CharSequence,length:Int= Toast.LENGTH_SHORT){
-        Toast.makeText(this,message,length).show();
+    fun MainActivity.toast(message: CharSequence, length: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(this, message, length).show();
     }
 }
