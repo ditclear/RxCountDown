cc7b752024482a66f16a5953e1f315ce7ce30bd6
vienan <vienan@163.com>
2016-02-18 12:56:21 +0800

    update file

--- app/src/main/java/vienan/app/countdown/MainActivity.java
+++ app/src/main/java/vienan/app/countdown/MainActivity.java
@@ -1,4 +1,4 @@
-﻿package vienan.app.countdown;
+package vienan.app.countdown;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -17,12 +17,15 @@ import rx.Observable;
 import rx.Observer;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     TextView tvCountDown;
     Subscription subscriptionCountDown;
-    DateTime currentDateTime,targetDateTime;
+//    DateTime currentDateTime,targetDateTime;
+
+//    String currentDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,19 +39,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initData() {
-        //targetDateTime=new DateTime(2015,12,25,11,2,0);
-        targetDateTime=new DateTime().plusHours(3);
+//        currentDate="2016-5-3T12:00";
+//        currentDate=new DateTime().plusHours(3).toString("yyyy-MM-dd HH:mm");
+//        targetDateTime=new DateTime(2015,12,25,11,2,0);
+//        targetDateTime=new DateTime().plusHours(3);
     }
 
     private void setUpView() {
         tvCountDown= (TextView) findViewById(R.id.tv_countdown);
         subscriptionCountDown=  Observable
-                .interval(0,1, TimeUnit.SECONDS, Schedulers.computation())
+                .interval(1, TimeUnit.SECONDS, Schedulers.computation())
+                .map(new Func1<Long, DateTime>() {
+                    @Override
+                    public DateTime call(Long s) {
+                        Log.d("s", ""+s);
+                        return new DateTime(2016,2,25,11,2,0);
+                    }
+                })
+                .map(new Func1<DateTime, Period>() {
+                    @Override
+                    public Period call(DateTime dateTime) {
+                        return new Interval(DateTime.now(),dateTime).toPeriod();
+                    }
+                })
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<Long>() {
+                .subscribe(new Observer<Period>() {
                     @Override
                     public void onCompleted() {
                         Log.d("onCompleted","task is completed");
+                        tvCountDown.setText("倒计时结束");
                     }
 
                     @Override
@@ -57,12 +76,9 @@ public class MainActivity extends AppCompatActivity {
                     }
 
                     @Override
-                    public void onNext(Long text) {
+                    public void onNext(Period period) {
                         Log.d("onNext","show countdown here");
-                        currentDateTime=DateTime.now();
-                        Interval interval=new Interval(currentDateTime,targetDateTime);
-                        Period period=interval.toPeriod();
-                        String countDownRText=period.getDays()+"天"+period.getHours()+"时"+period.getMinutes()+ "分"
+                        String countDownRText="剩 "+period.getDays()+"天"+period.getHours()+"时"+period.getMinutes()+ "分"
                                 +period.getSeconds()+"秒";
                         tvCountDown.setText(countDownRText);
                     }
@@ -73,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        subscriptionCountDown.unsubscribe();
+        if (!subscriptionCountDown.isUnsubscribed())
+            subscriptionCountDown.unsubscribe();
     }
 }
